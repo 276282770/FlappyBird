b19879af59d83e79934f36ade9a9e2ef768ee1c7
Codu <276282770@qq.com>
2018-12-12 11:11:30 +0800

    创建游戏

---
+++ Assets/Flappy Bird Style/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    public int playerIdx = 0;
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ Assets/Flappy Bird Style/Scripts/Loading.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loading : MonoBehaviour {
+
+    public GameObject panelIndex;
+    public GameObject panelSelectPlayer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnStart()
+    {
+       panelIndex.SetActive(false);
+        panelSelectPlayer.SetActive(true);
+    }
+    public void OnSelectPlayer0()
+    {
+        GameManager.Instance.playerIdx = 0;
+        GameManager.Instance.LoadScene("Main");
+    }
+    public void OnSelectPlayer1()
+    {
+        GameManager.Instance.playerIdx = 1;
+        GameManager.Instance.LoadScene("Main");
+    }
+
+}
